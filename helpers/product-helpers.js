ec9c13fe0b79426da63d7250e5a48b80ada70941
Adithyan M.S <adithyan2ms@gmail.com>
2022-02-16 00:03:41 +0530

    19.signup/login/logout--session

---
+++ helpers/product-helpers.js
@@ -0,0 +1,17 @@
+var db=require('../config/connection')
+var collection=require('../config/collections')
+module.exports={
+
+    addProduct:(product,callback)=>{
+        console.log(product);
+        db.gets().collection('product').insertOne(product).then((data)=>{
+            callback(data.insertedId)
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.gets().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
+    }
+}
